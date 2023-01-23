b78ba8668efed76b666ec44586d73df31db1df4e
Anton Anton <Toni95Kova@mail.com>
2023-01-24 01:02:19 +0200

    add new contetn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('ds')
+const form = document.querySelector('.world__form')
+const input = document.querySelector('.world__input')
+const formBtn = document.querySelector('.world__form-btn')
+
+formBtn.addEventListener('click', () => {
+  input.value = ''
+})
+input.addEventListener('focus', () => {
+  form.classList.add('world__form--active')
+})
+input.addEventListener('blur', () => {
+  form.classList.remove('world__form--active')
+})
