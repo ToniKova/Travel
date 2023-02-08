6b09af5d025f89a96ebd882a1835227d175981be
Anton Anton <Toni95Kova@mail.com>
2023-02-08 23:07:48 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.best__btn-next',
+    prevEl: '.best__btn-prev'
+  },
+})
+
 const form = document.querySelector('.world__form')
 const input = document.querySelector('.world__input')
 const formBtn = document.querySelector('.world__form-btn')
+const video = document.querySelectorAll('.best__video-title')
+
+
+video.forEach(item => {
+  item.addEventListener('focus', () => {
+    item.classList.add('video-z')
+  })
+})
+video.forEach(item => {
+  item.addEventListener('blur', () => {
+    item.classList.remove('video-z')
+  })
+})
 
 formBtn.addEventListener('click', () => {
   input.value = ''
